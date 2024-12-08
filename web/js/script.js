1858b46d33cb09db4d1ad3b8ed73de8503089710
Yang Si Jun <aersijun@gmail.com>
2024-12-09 00:53:36 +0200

    split up the nginx static browser to 8198 and gateway to 8197

--- web/js/script.js
+++ web/js/script.js
@@ -7,6 +7,22 @@ function sendRequest() {
 }
 
 function stopContainers() {
-    fetch('/stop')
-        .then(() => alert('System is stopping.'));
+    fetch('/state', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: 'SHUTDOWN'
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('System is stopping.');
+        } else {
+            alert('Failed to stop the system. Please try again.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred while trying to stop the system.');
+    });
 }
